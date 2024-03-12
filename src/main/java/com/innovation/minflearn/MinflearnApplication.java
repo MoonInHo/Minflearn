cb16992b36bd4f47aa264242a99b741dce0a7bcb
MoonInHo <ansdlsgh55@gmail.com>
2024-03-12 17:52:52 +0900

    first commit

---
+++ src/main/java/com/innovation/minflearn/MinflearnApplication.java
@@ -0,0 +1,13 @@
+package com.innovation.minflearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinflearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MinflearnApplication.class, args);
+    }
+
+}
